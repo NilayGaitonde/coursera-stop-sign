fd3d491e4752674456812fa6d3424f43107ad6be
Nilay Gaitonde <68230775+NilayGaitonde@users.noreply.github.com>
2024-04-04 21:01:23 +0530

    done

--- main.py
+++ main.py
@@ -20,18 +20,24 @@ def draw_boxes(model, frame, results,yoga_pose=None,score=None):
         if y2>710:
             y2=700
         cv2.rectangle(frame,(int(x1),int(y1)),(int(x2),int(y2)),(255,0,0),2)
-        print(x1,y1,x2,y2,confidence,label)
+        # print(x1,y1,x2,y2,confidence,label)
         cv2.putText(frame,f'{label} ({confidence:.2f}) is doing the {yoga_pose} pose ({score})',(int(x1),int(y1)),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2)
     return frame
         
 if __name__=='__main__':
     model = load_yolo()
-    load_img = "not_stop/132.jpg"
-    # load_img = "not_stop/102.jpg"
+    load_img = "stop/14.jpg"
+    img = cv2.imread(load_img)
     results = model(load_img)
-    cv2.imwrite('outputs/output7.jpg', results.render()[0])
     # results.show()
+    labels = list()
     for obj in results.xyxy[0]:
         confidence = obj[4].item() * 100
         label = model.names[int(obj[5])]
-        print(f'{label}: {confidence:.2f}%')
+        labels.append(label)
+    if 'stop sign' in labels:
+        cv2.putText(img,'Stop sign detected',(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2)
+    else:
+        cv2.putText(img,'Stop sign not detected',(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2)
+    cv2.imwrite('outputs/output11.jpg', img)
+    
